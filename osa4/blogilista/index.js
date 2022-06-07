3cb7fec4c5f9351434772685efa777821ee3faa2
Aikamoine <atte.laakso@helsinki.fi>
2022-06-07 10:01:45 +0300

    4.2 valmis

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -1,13 +1,9 @@
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const Blog = require('./models/blog')
 
-
+/*
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
     .then(() => {
@@ -20,24 +16,11 @@ mongoose.connect(mongoUrl)
 app.use(cors())
 app.use(express.json())
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
-
+const blogRouter = require('./controllers/blogs')
+app.use('/api/blogs', blogRouter)
+*/
+const server = http.createServer(app)
 const PORT = config.PORT
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     logger.info(`Server running on port ${PORT}`)
 })
