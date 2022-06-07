c0a68337f698790591aebf09941cd254da2ebcf0
Aikamoine <atte.laakso@helsinki.fi>
2022-06-07 09:37:46 +0300

    4.2 osasuoritus

--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -3,19 +3,19 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
-require('dotenv').config()
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const Blog = require('./models/blog')
 
-const blogSchema = mongoose.Schema({
-    title: String,
-    author: String,
-    url: String,
-    likes: Number
-})
 
-const Blog = mongoose.model('Blog', blogSchema)
-
-const mongoUrl = process.env.MONGODB_URI
+const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl)
+    .then(() => {
+        logger.info('connected to MongoDB')
+    })
+    .catch((error) => {
+        logger.error('error connecting to MongoDB:', error.message)
+    })
 
 app.use(cors())
 app.use(express.json())
@@ -37,7 +37,7 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
-const PORT = process.env.PORT
+const PORT = config.PORT
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    logger.info(`Server running on port ${PORT}`)
 })
